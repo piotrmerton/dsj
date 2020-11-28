6f560852d015a7d70ebed6866a22039726d37bb9
Piotr Merton <piotrmerton@gmail.com>
2020-11-28 14:58:58 +0100

    views: removed title from standings view, added title to competition position in jumper view

--- resources/views/standings.blade.php
+++ resources/views/standings.blade.php
@@ -1,12 +1,4 @@
 <h1>Standings view</h1>
 <small>resources/views/standings</small>
 
-
-<h2>{{ __('Klasyfikacja turnieju') }} <a href="{{ $data['tournament']['url'] }}">{{ $data['tournament']['name'] }}</a></h2>
-
-
-
-
-
-
 @include('components.standings.tournament', ['standings' => $data['standings'], 'tournament' => $data['tournament']])
