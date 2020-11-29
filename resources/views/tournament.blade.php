c5457e4cab93af15196a445195617b82acbd6914
Piotr Merton <piotrmerton@gmail.com>
2020-11-29 23:27:07 +0100

    added tabs to tournament view

--- resources/views/tournament.blade.php
+++ resources/views/tournament.blade.php
@@ -11,18 +11,36 @@
 
 			@include('components.breadcrumbs')
 
-			<div class="content__main content--competition">
-				<div class="viewport">
-
-					@include('components.calendar')
-
-					@include('components.standings.tournament', ['data' => $standings])
-				</div>
-			</div>
+			<div class="ui-tabs tabs--tournament">
+				<nav class="tabs__nav">
+					<ul class="nav__list">
+						<li class="list__item item--tab" data-tab-name="calendar">
+							<a class="do-toggle-tab" href="#">{{ __('Kalendarz')}}</a>
+						</li>
+						<li class="list__item item--tab tab--open" data-tab-name="standings">
+							<a class="do-toggle-tab" href="#">{{ __('Klasyfikacja generalna')}}</a>
+						</li>						
+					</ul>
+				</nav>
+				<section class="tabs__contents">
+					<ul class="tabs__list">
+						<li class="list__item item--tab" data-tab-name="calendar">
+							<a class="tab__title do-toggle-tab">{{ __('Kalendarz')}}</a>
+							<div class="tab__content">
+								@include('components.calendar')
+							</div>
+						</li>
+						<li class="list__item item--tab tab--open" data-tab-name="standings">
+							<a class="tab__title do-toggle-tab">{{ __('Wyniki konkursu')}}</a>
+							<div class="tab__content">
+								@include('components.standings.tournament', ['data' => $standings])
+							</div>
+						</li>
+					</ul>
+				</section>
+			</div><!-- /tabs -->
+	
 		</div>
 	</div>
-
-
-
 
 @endsection
