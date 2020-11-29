583e0c4b7f34d04412bb64dc1e21fe0575a49a82
Piotr Merton <piotrmerton@gmail.com>
2020-11-29 02:58:11 +0100

    competition view: moved tournament standings to ui tabs

--- resources/views/competition.blade.php
+++ resources/views/competition.blade.php
@@ -14,17 +14,20 @@
 			<div class="ui-tabs tabs--competition">
 				<nav class="tabs__nav">
 					<ul class="nav__list">
-						<li class="list__item item--tab" data-tab-name="qualiifcations">
+						<li class="list__item item--tab" data-tab-name="qualifications">
 							<a class="do-toggle-tab" href="#">{{ __('Wyniki kwalifikacji')}}</a>
 						</li>
 						<li class="list__item item--tab tab--open" data-tab-name="final">
 							<a class="do-toggle-tab" href="#">{{ __('Wyniki konkursu')}}</a>
 						</li>
+						<li class="list__item item--tab" data-tab-name="tournament">
+							<a class="do-toggle-tab" href="#">{{ __('Klasyfikacja generalna')}}</a>
+						</li>						
 					</ul>
 				</nav>
 				<section class="tabs__contents">
 					<ul class="tabs__list">
-						<li class="list__item item--tab" data-tab-name="qualiifcations">
+						<li class="list__item item--tab" data-tab-name="qualifications">
 							<a class="tab__title do-toggle-tab">{{ __('Wyniki kwalifikacji')}}</a>
 							<div class="tab__content">
 								@include('components.standings.qualifications', ['competition' => $competition])
@@ -35,12 +38,16 @@
 							<div class="tab__content">
 								@include('components.standings.competition', ['competition' => $competition])
 							</div>
-						</li>						
+						</li>
+						<li class="list__item item--tab" data-tab-name="tournament">
+							<a class="tab__title do-toggle-tab">{{ __('Klasyfikacja generalna')}}</a>
+							<div class="tab__content">
+								@include('components.standings.tournament', ['data' => $tournament_standings])
+							</div>
+						</li>
 					</ul>
 				</section>
 			</div><!-- /tabs -->
-
-			@include('components.standings.tournament', ['data' => $tournament_standings])
 
 		</div><!-- /viewport -->
 	</div><!-- /content__main -->
